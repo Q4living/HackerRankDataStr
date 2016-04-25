e520dc0855381e5117071ed9c8e61ff1df2703d4
Q4living <thomasqian@yahoo.com>
2016-04-25 16:29:03 +0800

    Update Arrays- DS.r

--- Arrays- DS.r
+++ Arrays- DS.r
@@ -1,13 +1,14 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-input<-file('stdin', 'r')
+input <- file('stdin', 'r')
 row <- as.numeric(readLines(input, n=1))
 input <- readLines(input, n=2)
 dataset <- as.numeric(strsplit(input,split=" ")[[1]])
 list <- NULL #initate list with nothing in it
+i <- row #initate the iteration for the while loop
 
-while (row > 0){
-    list <- c(list, dataset[[row]])    
-    row <- row -1
+while (i > 0){
+    list <- c(list, dataset[[i]])    
+    i <- i - 1
 }
 
-write(list, stdout())
+write(list, stdout(),ncolumns = row)
